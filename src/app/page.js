aaa3d4280de137728c96728bcf7547fcd46200ea
Tsotneshonia1 <cotneshonia.17@gmail.com>
2024-02-16 00:41:09 +0400

    adding_navbar_and_responsive

--- src/app/page.js
+++ src/app/page.js
@@ -2,10 +2,10 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  throw new Error('Error in Home');
+ 
   return (
-    <main >
+ 
       <div>Homepage</div>
-    </main>
+
   );
 }
--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,34 +1,55 @@
-import React from "react";
+"use client";
+
 import Link from "next/link";
+import React, { useState } from "react";
+import styles from "./links.module.css";
 
+const links = [
+  {
+    title: "Homepage",
+   url: "/",
+  },
+
+  {
+    title: "About",
+    url: "/about",
+  },
+  {
+    title: "Contact",
+    url: "/contact",
+  },
+
+  {
+    title: "Blog",
+    url: "/blog",
+  },
+];
 function Links() {
-  const links = [
-    {
-      title: "Homepage",
-      url: "/",
-    },
+  const [open, setOpen] = useState(false);
 
-    {
-      title: "About",
-      url: "/about",
-    },
-    {
-      title: "Contact",
-      url: "/contact",
-    },
-
-    {
-      title: "Blog",
-      url: "/blog",
-    },
-  ];
-
-  return;
-  <div>
-    {links.map((link) => (
-      <Link href={link.url}>{link.title}</Link>
-    ))}
-  </div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.links}>
+        {links.map((link) => (
+          <Link href={link.url}>{link.title}</Link>
+        ))}
+        <button className={styles.logout}>Logout</button>
+      </div>
+      <button
+        className={styles.menuButton}
+        onClick={() => setOpen((prev) => !prev)}
+      >
+        Menu
+      </button>
+      {open && (
+        <div className={styles.mobileLinks}>
+          {links.map((link) => (
+            <Link href={link.url}>{link.title}</Link>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Links;
