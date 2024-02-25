4b93f6e0bbbd232c2c693a21ced5160bdbc3684d
Tsotneshonia1 <cotneshonia.17@gmail.com>
2024-02-25 23:14:17 +0400

    adding blogPage

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -3,14 +3,22 @@ import styles from "./blog.module.css";
 import PostCard from "@/components/postCard/postCard";
 
 function BlogPage() {
-  return <div className={styles.container}>
-    <div className={styles.post}>
-    <PostCard/>
-    <PostCard/>
-    <PostCard/>
-    <PostCard/>
+  return (
+    <div className={styles.container}>
+      <div className={styles.post}>
+        <PostCard />
+      </div>
+      <div className={styles.post}>
+        <PostCard />
+      </div>
+      <div className={styles.post}>
+        <PostCard />
+      </div>
+      <div className={styles.post}>
+        <PostCard />
+      </div>
     </div>
-  </div>;
+  );
 }
 
 export default BlogPage;
